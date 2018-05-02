1e570897bd3c20a37b8bb296303e7c97c1bff011
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-05-03 00:29:35 +0200

    Basic structure
    Finished basic view of project structure

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- SERVER -->
+<?php
+    echo 'No bryy :D';
+?>
+<!-- CLIENT -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>= KGB Platoon =</title>
+    <!-- additional libraries -->
+    <script src="./static/lib/jquery.js"></script>
+    <!-- own include -->
+    <sript src="./dist/scripts/main.min.js"></script>
+    <link rel="stylesheet" href="./dist/styles/style.min.css">
+</head>
+<body>
+    
+</body>
+</html>
